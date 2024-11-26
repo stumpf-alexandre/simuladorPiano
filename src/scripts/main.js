68f80b0f2783372cfe98115138da2c16952f6943
stumpf-alexandre <als.stumpf@gmail.com>
2024-11-25 23:37:17 -0300

    linkando audio e movimento as teclas

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,18 @@ let audio = new Audio("src/tunes/a.wav");
 const playTune = (key) => {
     audio.src = `src/tunes/${key}.wav`;
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
 };
 
 pianoKeys.forEach((key) => {
     key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+document.addEventListener("keydown", (e) => {
+    playTune(e.key);
 });
