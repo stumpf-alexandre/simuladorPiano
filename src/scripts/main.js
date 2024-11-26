3b04e633a9150bca9a33e4c51ae12248d8003b40
stumpf-alexandre <als.stumpf@gmail.com>
2024-11-26 00:07:24 -0300

    fazendo funcionar a tecla de tirar as letras dos teclados

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@ const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
 const volumeSlider = document.querySelector(".volume-slider input");
 
+const keysCheck = document.querySelector(".keys-check input");
+
 let audio = new Audio("src/tunes/a.wav");
 
 let mapedKeys = [];
@@ -33,4 +35,10 @@ const handleVolume = (e) => {
     audio.volume = e.target.value;
 };
 
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"));
+};
+
 volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHideKeys);
